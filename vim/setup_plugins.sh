1dbbc932d7445b75f307a067019c6383c5b1f2ea
TJ Bearse <thomasb@qualtrics.com>
2017-07-04 00:38:15 -0700

    emacs update; plugin install fix

--- vim/setup_plugins.sh
+++ vim/setup_plugins.sh
@@ -1,4 +1,8 @@
-ex -n -es -c 'PluginInstall' -c 'exit'
+mkdir -p ~/.vim/bundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+vim +PluginInstall +qall
+mkdir -p ~/.vim/bundle/YouCompleteMe
 (cd ~/.vim/bundle/YouCompleteMe && \
    git submodule update --init --recursive && \
    ./install.py)
