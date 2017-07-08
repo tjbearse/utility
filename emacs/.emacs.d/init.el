eef8fe4b05aae893df90231b9c3bb720e97f3941
TJ Bearse <thomasb@qualtrics.com>
2017-07-08 09:12:50 -0700

    add god mode

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,2 +1,19 @@
 (package-initialize)
 (org-babel-load-file "~/.emacs.d/configuration.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(custom-enabled-themes (quote (wombat)))
+ '(package-selected-packages
+   (quote
+    (evil-god-state god-mode fiplr web-mode use-package evil))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
