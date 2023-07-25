1271281ba8dc601e93f8bbf3e8283e9fa7803268
TJ Bearse <5686806+tjbearse@users.noreply.github.com>
2023-07-25 08:38:22 -0700

    kitty config

---
+++ config/kitty/kitty.sh
@@ -0,0 +1,7 @@
+
+# TODO if source this if term is xterm-kitty
+# ImageMagick must be installed for icat to work.
+alias icat="kitty +kitten icat --align=left"
+alias isvg="rsvg-convert | icat"
+
+alias ssh="kitty +kitten ssh"
